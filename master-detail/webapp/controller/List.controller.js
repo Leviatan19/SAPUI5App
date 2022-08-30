02eaaef03965ecfd67d8dc0b7fab692df8ff4d6f
Sebastian Urbacki <urbacki.sebastian@gmail.com>
2022-08-30 09:06:06 +0000

    Added update buttons

--- master-detail/webapp/controller/List.controller.js
+++ master-detail/webapp/controller/List.controller.js
@@ -92,6 +92,86 @@ sap.ui.define([
             this._updateListItemCount(oEvent.getParameter("total"));
         },
 
+        onUpdateClick: function(oEvent){
+            var oModel = this.getView().getModel();
+
+            const clickedItemContext = oEvent.getSource().getBindingContext()
+            const clickedItemPath = clickedItemContext.getPath();
+            const clickedItemObject = clickedItemContext.getObject();
+            const prevName = clickedItemObject.Name;
+
+            this.oApproveDialog = new Dialog({
+                type: DialogType.Message,
+                title: "Update",
+                content: new Input({
+                    id: "nameInput",
+                    value: prevName
+                }),
+                beginButton: new Button({
+                    type: ButtonType.Emphasized,
+                    text: "Submit",
+                    press: function () {
+                        const newName = this.oApproveDialog.getContent()[0].getValue()
+                        oModel.read("/Categories", {
+                            success: function (data) {
+                                console.log(data.results)
+                                const isNameFree = !data.results?.find(cat => cat.Name === newName);
+
+                                if (isNameFree) {
+                                    this._updateConfirmDialog(prevName, newName, clickedItemPath);
+                                } else {
+                                    console.log("is not free")
+                                    MessageBox.error("Category with that name already exists!", {
+                                        title: "Error"
+                                    })
+                                }
+                                this.oApproveDialog.destroy();
+
+                            }.bind(this)
+                        });
+                    }.bind(this)
+                }),
+                endButton: new Button({
+                    text: "Cancel",
+                    press: function() {
+                        this.oApproveDialog.destroy();
+                    }.bind(this)
+                })
+            });
+            this.oApproveDialog.open();
+        },
+        _updateConfirmDialog: function(prevName, newName, clickedItemPath){
+            var oModel = this.getView().getModel();
+
+            this.oConfirmDialog = new Dialog({
+                type: DialogType.Message,
+                title: "Confirmation",
+                content: new Text({
+                    text: `Are you sure you want to rename category from ${prevName} to ${newName}?`
+                }),
+                beginButton: new Button({
+                    type: ButtonType.Accept,
+                    text: "Yes",
+                    press: function () {
+                        var oCat = {"Name": newName}
+                        oModel.update(clickedItemPath, oCat, {
+                            merge: true, /* if set to true: PATCH/MERGE/ */
+                            success: function () {MessageToast.show("Success!");},
+                            error: function (oError) {MessageToast.show("Something went wrong!");}
+                        });
+                        this.oConfirmDialog.destroy();
+                    }.bind(this)
+                }),
+                endButton: new Button({
+                    text: "No",
+                    type: ButtonType.Reject,
+                    press: function () {
+                        this.oConfirmDialog.destroy();
+                    }.bind(this)
+                })
+            });
+            this.oConfirmDialog.open();
+        }
         /**
          * Event handler for the list search field. Applies current
          * filter value and triggers a new search. If the search field's
